d0d2073581e7711344bc4757f1b4b75e968ac7a9
martea <1642485710@qq.com>
2020-11-22 16:02:04 +0800

    学习使用路由router相关

---
+++ 09-learnRouter/testvuecli-router/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+// 1.通过Vue.use（插件），安装插件
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+// 2.创建VueRouter对象
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ 09-learnRouter/testvuecli-router/src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>使用vue-router的步骤</h1>
+    <h2>第一步：创建路由组建，详见router文件夹下面的index.js</h2>
+    <h2>第二步：配置路由映射：组件和路径的映射关系配置</h2>
+    <h2>第三步：使用路由，通过&ltrouter-link&gt和&ltrouter-view&gt标签使用</h2>
+  </div>
+</template>
