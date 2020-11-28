fbfc3fa77e39042d5ad67865e3b0cef7a9d8bff6
martea <1642485710@qq.com>
2020-11-28 13:21:20 +0800

    使用$route获取传参

--- 09-learnRouter/testvuecli-router/src/views/Profile.vue
+++ 09-learnRouter/testvuecli-router/src/views/Profile.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h2>我是个人简历页</h2>
+        <h2>{{$route.query.name}}</h2>
+        <h2>{{$route.query.age}}</h2>
+        <h2>{{$route.query.sex}}</h2>
     </div>
 </template>
 <script>
